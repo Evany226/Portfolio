c6b1824fc35383a9a9cb99e756cf26a0397a7393
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-08-24 15:16:49 -0400

    added text swap effect

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 console.log("connected");
+
+const introList = ["Welcome!", "Hello!", "Hey there!"];
+
+const cycle = document.querySelector(".intro-title");
+let i = 0;
+const cycleText = () => {
+  cycle.innerHTML = introList[i];
+  i = ++i % introList.length;
+};
+cycleText();
+setInterval(cycleText, 3000);
