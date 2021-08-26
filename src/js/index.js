ecdef6953c1acd67836b17c4af87a68deaf1243e
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-08-26 13:52:34 -0400

    added back cycling text
    cc

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 console.log("connected");
 
+//Cycles Text on Introduction page
+const introList = ["Welcome!", "Hello!", "Hey there!"];
+const cycle = document.querySelector(".intro-title");
+
+let i = 0;
+const cycleText = () => {
+  cycle.innerHTML = introList[i];
+  i = (i + 1) % introList.length;
+};
+cycleText();
+setInterval(cycleText, 4000);
+
 // Triggers the hamburger navbar on mobile//
 const toggleButton = document.getElementsByClassName("navbar-toggle")[0];
 const navbarLinks = document.getElementsByClassName("navbar-menu")[0];
