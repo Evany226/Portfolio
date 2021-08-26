948d8d7db461f22f899f9dc7b8cd6fa7d2effcb8
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-08-26 13:47:15 -0400

    completed nav bar
    cc

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,8 @@
 console.log("connected");
 
-//Constantly swaps text on intro section
-const introList = ["Welcome!", "Hello!", "Hey there!"];
-
-const cycle = document.querySelector(".intro-title");
-let i = 0;
-const cycleText = () => {
-  cycle.innerHTML = introList[i];
-  i = (i + 1) % introList.length;
-};
-cycleText();
-setInterval(cycleText, 4000);
-
-// Triggers the hamburger navbar on mobile
-const toggleButton = document.getElementByClassName("navbar-toggle");
-const navbarLinks = document.getElementsByClassName("navbar-menulinks");
+// Triggers the hamburger navbar on mobile//
+const toggleButton = document.getElementsByClassName("navbar-toggle")[0];
+const navbarLinks = document.getElementsByClassName("navbar-menu")[0];
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
