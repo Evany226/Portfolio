8c2aedb450c0b6474deb7f84343a3f36988d79a9
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-08-28 11:51:24 -0400

    c

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,5 @@
 console.log("connected");
 
-//Cycles Text on Introduction page
-const introList = ["Welcome!", "Hello!", "Hey there!"];
-const cycle = document.querySelector(".intro-title");
-
-let i = 0;
-const cycleText = () => {
-  cycle.innerHTML = introList[i];
-  i = (i + 1) % introList.length;
-};
-cycleText();
-setInterval(cycleText, 4000);
-
 // Triggers the hamburger navbar on mobile//
 const toggleButton = document.getElementsByClassName("navbar-toggle")[0];
 const navbarLinks = document.getElementsByClassName("navbar-menu")[0];
